6d2fd5daecff3b43dd9930ce43b0bb0ef1a5b371
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-15 21:43:28 -0700

    added Details to Router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from '../../containers/Main';
+import Details from '../details/Details';
+
 const App = () => {
   return (
     <>
       <Router>
         <Switch>
           <Route path="/" exact component={Main} />
+          <Route path="/details/:name" component={Details} />
         </Switch>
       </Router>
     </>
