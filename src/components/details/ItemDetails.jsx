c3b9859dda779522dcdb60d7d59cbb5ca357579a
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-16 10:12:38 -0700

    updated Details and snapshots

--- src/components/details/ItemDetails.jsx
+++ src/components/details/ItemDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './styles/ItemDetails.css';
 
 const ItemDetails = ({ character }) => {
   [character] = character;
@@ -7,6 +8,13 @@ const ItemDetails = ({ character }) => {
     <div>
       {character.name}
       <img src={character.image} />
+      <div className={styles.details}>
+        <p>Quote: {character.quote}</p>
+        <p>Personality: {character.personality}</p>
+        <p>Birthday: {character.birthday}</p>
+        <p>Species: {character.species}</p>
+        <p>Phrase: {character.phrase}</p>
+      </div>
     </div>
   );
 };
