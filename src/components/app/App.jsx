659064847c7374880a02a83d6e1abbbdb97da2ff
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-15 21:18:57 -0700

    outline Main.jsx and link to list.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from '../../containers/Main';
-
 const App = () => {
   return (
     <>
-      <div className="main">
-        <Router>
-          <Switch>
-            <Route path="/" exact component={Main} />
-          </Switch>
-        </Router>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Main} />
+        </Switch>
+      </Router>
     </>
   );
 };
-
 export default App;
